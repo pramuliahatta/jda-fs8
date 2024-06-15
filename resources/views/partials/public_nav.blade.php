0179efc71f1ea76aca1321efdb60c96d98d493cf
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-06-15 14:27:41 +0700

    add link to navbar

--- resources/views/partials/public_nav.blade.php
+++ resources/views/partials/public_nav.blade.php
@@ -91,7 +91,7 @@
             </span>
         </a>
         <div class="flex items-center lg:order-2">
-            <a href="#"
+            <a href="{{ route('login') }}"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
                 Log in
             </a>
@@ -126,13 +126,13 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#"
+                    <a href="{{ route('services') }}"
                         class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
                         Layanan
                     </a>
                 </li>
                 <li>
-                    <a href="#"
+                    <a href="{{ route('products') }}"
                         class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
                         Pasar Desa
                     </a>
