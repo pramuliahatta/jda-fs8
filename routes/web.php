9270df1263c1d8586ece9cc755c59ac85b720d0e
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-06-15 14:41:01 +0700

    add name route home

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/login', function () {
     return view('auth.login');
