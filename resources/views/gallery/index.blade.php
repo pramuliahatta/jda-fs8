bdc2ced6b34c2239189c25e4bd759e2933e98518
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-06-16 18:03:25 +0700

    fix pagination

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -11,9 +11,7 @@
                     Lihatlah keindahan dan kehidupan sehari-hari di Desa Cidadap melalui galeri foto kami.
                 </p>
             </div>
-            <div class="grid gap-8 mb-6 lg:mb-16">
-
-
+            <div class="grid gap-8">
                 <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
                     <div class="grid gap-4">
                         <div>
@@ -77,7 +75,7 @@
 
             </div>
             <nav aria-label="Page navigation example">
-                <ul class="flex justify-end items-center -space-x-px h-8 text-sm">
+                <ul class="mt-8 flex justify-end items-center -space-x-px h-8 text-sm">
                     <li>
                         <a href="#"
                             class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
