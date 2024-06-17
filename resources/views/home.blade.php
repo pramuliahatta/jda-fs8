fe81d04a8fd40e6540000807ab85a8c27612b6d3
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-06-17 08:41:15 +0700

    add lihat semua galeri button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -424,9 +424,7 @@
                     Lihatlah keindahan dan kehidupan sehari-hari di Desa Cidadap melalui galeri foto kami.
                 </p>
             </div>
-            <div class="grid gap-8 mb-6 lg:mb-16">
-
-
+            <div class="grid gap-8">
                 <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
                     <div class="grid gap-4">
                         <div>
@@ -496,7 +494,18 @@
                         </div>
                     </div>
                 </div>
-
+            </div>
+            <div class="flex justify-end my-4">
+                <a href="#"
+                    class="inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
+                    Lihat Semua Galeri
+                    <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </a>
             </div>
         </div>
     </section>
