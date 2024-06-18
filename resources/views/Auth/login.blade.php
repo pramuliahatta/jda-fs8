f5ae4c897381017fd037b1c71daf9c4c97a637dc
nurul <chynrl18@gmail.com>
2024-06-18 14:41:11 +0700

    menambah logo pada login

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -1,19 +1,19 @@
 <x-layout>
     <section class="bg-gray-50 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                {{-- <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"> --}}
-                Pemerintahan Desa Cidadap
-            </a>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Lambang_Kabupaten_Majalengka.svg" class="h-12, w-12"
+            alt="logo" />
             <div
-                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                class="w-full bg-white rounded-lg shadow dark:border md:mt-8 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                {{-- <div class = "p-2 space-y-4 md:space-y-6 sm:p-8"></div> --}}
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <div class="flex justify-center items-center">
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Masuk ke Akun Anda
+                        Log in
                     </h1>
+                    </div>
                     <form class="space-y-4 md:space-y-6" action="{{ route('authenticate') }}" method="POST">
                         @csrf
-
                         <div>
                             <label for="email"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Anda</label>
@@ -42,7 +42,7 @@
 
 
                         <button type="submit"
-                            class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Masuk</button>
+                            class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log in</button>
                     </form>
                 </div>
             </div>
