35803f5887feb5c8cd598a8f11dfbdc4a053506d
Adi Septiandi <adiseptiandi06@student.unsia.ac.id>
2024-06-24 09:48:53 +0700

    just try

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\FileController;
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\GalleryController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResources([
+    "galleries" => GalleryController::class,
+    "files" => FileController::class,
+    "articles" => ArticleController::class,
+]);
+
+// Route::post("/galleries/edit/{id}", [GalleryController::class, "update"]);
