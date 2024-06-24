5bf58bae398d7bef2e0508d683629fb36a45f4a1
Adi Septiandi <adiseptiandi06@student.unsia.ac.id>
2024-06-24 10:26:16 +0700

    just try

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-// Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
-// Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
-// Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/services', [FileController::class, 'index'])->name('services');
 
