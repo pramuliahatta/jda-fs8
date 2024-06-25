bb9275a2b6c70648f52fcec4c508a30f903233b7
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-06-25 10:02:54 +0700

    re-style navigation and sidebar

--- resources/views/components/sidebar-link.blade.php
+++ resources/views/components/sidebar-link.blade.php
@@ -2,7 +2,7 @@
 
 <li>
     <a href="{{ $href }}"
-        {{ $attributes->merge(['class' => 'flex items-center p-2 text-base font-medium rounded-lg group' . ($active ? ' text-blue-700 bg-gray-100 dark:bg-gray-700 dark:text-white' : ' text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white')]) }}>
+        {{ $attributes->merge(['class' => 'flex items-center p-2 text-base font-medium rounded-lg group' . ($active ? ' text-green-500 bg-gray-100 dark:bg-gray-700 dark:text-white' : ' text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white')]) }}>
         {{ $slot }}
     </a>
 </li>
