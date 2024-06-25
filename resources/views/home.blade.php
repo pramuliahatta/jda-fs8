ca0c37c4be09ea965ed1b8336086f8e0bc83d947
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-06-25 10:40:28 +0700

    fix services page style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,16 +39,16 @@
         <div class="bg-green-50 rounded-lg max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
             <dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
                 <div class="flex flex-col items-center justify-center">
-                    <dt class="mb-2 text-3xl md:text-4xl font-extrabold">5000+</dt>
-                    <dd class="font-light text-gray-700 dark:text-gray-400">penduduk</dd>
+                    <dt class="mb-2 text-3xl md:text-4xl font-extrabold">2.785+</dt>
+                    <dd class="font-light text-gray-700 dark:text-gray-400">jumlah penduduk</dd>
                 </div>
                 <div class="flex flex-col items-center justify-center">
-                    <dt class="mb-2 text-3xl md:text-4xl font-extrabold">150km<sup>2</sup></dt>
+                    <dt class="mb-2 text-3xl md:text-4xl font-extrabold">274.548,5 Ha</dt>
                     <dd class="font-light text-gray-700 dark:text-gray-400">luas wilayah</dd>
                 </div>
                 <div class="flex flex-col items-center justify-center">
-                    <dt class="mb-2 text-3xl md:text-4xl font-extrabold">10 RT/5 RW</dt>
-                    <dd class="font-light text-gray-700 dark:text-gray-400">jumlah RT/RW</dd>
+                    <dt class="mb-2 text-3xl md:text-4xl font-extrabold">826</dt>
+                    <dd class="font-light text-gray-700 dark:text-gray-400">jumlah KK</dd>
                 </div>
             </dl>
         </div>
