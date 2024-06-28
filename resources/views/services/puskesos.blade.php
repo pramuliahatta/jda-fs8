2694198aa020d9dc6de7700a8c42c06701616f00
nurul <chynrl18@gmail.com>
2024-06-28 13:20:03 +0700

    make puskesos page

---
+++ resources/views/services/puskesos.blade.php
@@ -0,0 +1,103 @@
+<x-layout>
+    <x-slot name="title">Puskesos</x-slot>
+    <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+            <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+                <h1 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+                    Layanan Sosial
+                </h1>
+                <p class="font-light text-gray-700 lg:mb-16 sm:text-xl dark:text-gray-400">
+                    Layanan ini menyediakan informasi mengenai Puskesos
+                </p>
+            </div>
+
+            
+            <section class="bg-white dark:bg-gray-900">
+                <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                    <div class="font-light text-gray-700 sm:text-lg dark:text-gray-400">
+                        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Apa itu Puskesos?</h2>
+                        <p class="mb-4">
+                            Pusat Kesejahteraan Sosial (Puskesos) adalah lembaga yang dibentuk oleh pemerintah di tingkat desa atau kelurahan untuk memberikan layanan sosial kepada masyarakat. 
+                            Puskesos berfungsi sebagai pusat informasi, rujukan, dan layanan sosial terpadu yang bertujuan untuk meningkatkan kesejahteraan sosial masyarakat. 
+                            Layanan yang disediakan oleh Puskesos meliputi bantuan sosial, pelayanan kesehatan, pendidikan, dan pemberdayaan ekonomi. 
+                            Puskesos juga berperan dalam mendeteksi dan menangani masalah sosial seperti kemiskinan, kekerasan dalam rumah tangga, dan masalah kesehatan mental. 
+                            Melalui Puskesos, diharapkan masyarakat dapat lebih mudah mengakses berbagai bentuk bantuan dan pelayanan sosial yang mereka butuhkan.
+                        </p>
+                        <p>Lembaga Puskesos Desa Cidadap menyediakan layanan berupa pengajuan KIP (Kartu Indonesia Pintar) dan pengajuan BPJS/KIS (UHC)</p>
+                    </div>
+            </section>
+
+            <section class="bg-green-50 dark:bg-gray-900">
+                
+                <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+                    <div class="gap-4 mt-8">
+                        <img class="w-full rounded-lg" src="https://th.bing.com/th/id/R.d3f0d5ce585554dea5a6e9e96df33a0d?rik=0XDh8ahCdQENIQ&riu=http%3a%2f%2fselat-buleleng.desa.id%2fassets%2ffiles%2fartikel%2fsedang_1567167348Kartu-Indonesia-Sehat-854x540.jpg&ehk=fLwKze%2f7CdHtUr%2fZbHZwuLKc%2fNtz5TQapMoarr7pisI%3d&risl=&pid=ImgRaw&r=0" 
+                        alt="KIS">
+                        {{-- <img class="mt-4 w-full lg:mt-10 rounded-lg" 
+                        src="https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/8c7d2654-0527-43a3-88aa-bce9c5421a9f/1133824147/cara-cek-kartu-bpjs-kesehatan-screenshot.png" 
+                        alt="office content 2"> --}}
+                    </div>
+                    <div class="font-light text-gray-700 sm:text-lg dark:text-gray-400">
+                        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Pengajuan BPJS/KIS</h2>
+                        <p class="mb-4">BPJS (Badan Penyelenggara Jaminan Sosial) Kesehatan adalah badan hukum yang dibentuk untuk menyelenggarakan program Jaminan Kesehatan Nasional (JKN) di Indonesia. 
+                            Kartu Indonesia Sehat (KIS) merupakan bagian dari program ini, yang memberikan akses pelayanan kesehatan bagi seluruh masyarakat Indonesia, terutama bagi mereka yang kurang mampu. 
+                            Dengan KIS, pemegang kartu bisa mendapatkan layanan kesehatan di fasilitas kesehatan yang bekerjasama dengan BPJS Kesehatan, tanpa harus membayar biaya layanan langsung, 
+                            karena biayanya sudah ditanggung oleh pemerintah melalui skema JKN. 
+                            Program ini bertujuan untuk mewujudkan sistem kesehatan yang lebih inklusif dan merata di seluruh Indonesia.
+                        </p>
+                        <p>Persyaratan pengajuan BPJS/KIS</p>
+                        <ul class="max-w-md space-y-1 text-gray-700 list-disc list-inside dark:text-gray-400">
+                            <li>
+                                SKTM (Surat Keterangan Tidak Mampu)
+                            </li>
+                            <li>
+                                Fotokopi KTP
+                            </li>
+                            <li>
+                                Fotokopi KK
+                            </li>
+                            <li>
+                                Surat Keterangan Rawat Inap/Rawat Jalan/Rujukan
+                            </li>
+                        </ul>
+                        <p>Warga bisa membawa persyartan tersebut ke kantor Desa Cidadap.</p>
+                    </div>
+                    
+                </div>
+            </section>
+
+            <section class="bg-white dark:bg-gray-900 rounded-lg">
+                <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+                    <div class="font-light text-gray-700 sm:text-lg dark:text-gray-400">
+                        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Pengajuan KIP</h2>
+                        <p class="mb-4">Kartu Indonesia Pintar (KIP) adalah program pemerintah Indonesia yang bertujuan untuk memberikan akses pendidikan yang lebih merata kepada anak-anak dari keluarga kurang mampu. 
+                            Melalui KIP, pemerintah memberikan bantuan berupa dana pendidikan yang dapat digunakan untuk keperluan sekolah, seperti membeli buku, seragam, alat tulis, dan membayar biaya sekolah. 
+                            Program ini diharapkan dapat membantu mengurangi angka putus sekolah dan meningkatkan partisipasi pendidikan, sehingga semua anak Indonesia dapat memperoleh pendidikan yang layak dan berkualitas.
+                        </p>
+                        <p>Warga dapat mendapatkan kartu tersebut dengan melakukan pengajuan pembuat KIP dengan persyaratan sebagai berikut.</p>
+                        <ul class="max-w-md space-y-1 text-gray-700 list-disc list-inside dark:text-gray-400">
+                            <li>
+                                SKTM (Surat Keterangan Tidak Mampu)
+                            </li>
+                            <li>
+                                KTP
+                            </li>
+                            <li>
+                                KK
+                            </li>
+                        </ul>
+                        <p>Warga bisa membawa persyartan tersebut ke kantor Desa Cidadap.</p>
+
+                    </div>
+                    <div class=" gap-4 mt-8">
+                        <img class="w-full rounded-lg" src="https://asset-3.tstatic.net/jualbeli/img/njajal/2023/1/Pendaftaran-Bantuan-KIP-Kuliah-2023-Segera-Dibuka--Ini-Besaran--Syarat--dan-Cara-Daftarnya-master-1828019094.jpg" 
+                        alt="office content 1">
+                    </div>
+                </div>
+            </section>
+
+
+
+        </div>
+    </section>
+</x-layout>
