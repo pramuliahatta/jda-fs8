62984d7dfdd6307c87e9e716c31efaaeece931a8
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-06-29 11:40:09 +0700

    add category badge

--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -1,4 +1,7 @@
-<!-- resources/views/components/article.blade.php -->
+@php
+    $categoryClass = $detail['category'] == 'Acara' ? 'bg-red-100 text-red-600' : 'bg-green-100 text-green-600';
+@endphp
+
 <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
     <header class="mb-4 lg:mb-6 not-format">
         <address class="flex items-center mb-6 not-italic">
@@ -19,6 +22,17 @@
         <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
             {{ $detail['title'] }}
         </h1>
+        <span
+            class="
+                        {{ $categoryClass }}
+                          text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+            <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z">
+                </path>
+            </svg>
+            {{ $detail['category'] }}
+        </span>
     </header>
 
     {!! $leadParagraphHtml !!}
