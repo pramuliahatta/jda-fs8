5c2d318cff5103ad813a56412f9e67ef3f01bc36
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-06-30 17:24:57 +0700

    fix gallery lightbox behavior

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -20,6 +20,21 @@
                         <div
                             class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                             <div class="w-full md:w-1/2">
+                                <label for="simple-search" class="sr-only">Cari</label>
+                                <div class="relative w-full">
+                                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                        <svg aria-hidden="true" class="w-5 h-5 text-gray-700 dark:text-gray-400"
+                                            fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                    <input type="text" id="simple-search" name="search"
+                                        value="{{ request('search') }}"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-400 focus:border-green-400 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Cari">
+                                </div>
                             </div>
 
                             <div
@@ -66,7 +81,7 @@
                                 </div>
 
                                 <!-- Add reset button conditionally -->
-                                @if (request('sort'))
+                                @if (request('search') || request('sort'))
                                     <button type="button" id="resetButton"
                                         class="mt-4 w-full text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
                                         Hapus Filter
@@ -140,6 +155,7 @@
         </div>
     </div>
     {{-- LIGHTBOX SECTION END --}}
+
 </x-layout>
 
 
