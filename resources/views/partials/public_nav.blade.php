8a653d5aa38ed732f306d5d14b8caaf1c736a827
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-07-02 08:52:00 +0700

    add dropdown navigation link

--- resources/views/partials/public_nav.blade.php
+++ resources/views/partials/public_nav.blade.php
@@ -47,7 +47,33 @@
             <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                 <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">Beranda</x-nav-link>
                 <x-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')">Tentang Kami</x-nav-link>
-                <x-nav-link href="{{ route('services') }}" :active="request()->routeIs('services')">Layanan</x-nav-link>
+                {{-- <x-nav-link href="{{ route('services') }}" :active="request()->routeIs('services')">Layanan</x-nav-link> --}}
+                <li>
+                    <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+                        class="{{ request()->routeIs('services') || request()->routeIs('puskesos') ? 'block py-2 pr-4 pl-3 text-white rounded bg-green-500 lg:bg-transparent lg:text-green-500 lg:p-0 dark:text-white' : 'block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-green-500 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700' }} flex items-center justify-between w-full py-2 px-3"
+                        aria-current="{{ request()->routeIs('services') || request()->routeIs('puskesos') ? 'page' : '' }}">
+                        Layanan
+                        <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 4 4 4-4" />
+                        </svg></button>
+                    <!-- Dropdown menu -->
+                    <div id="dropdownNavbar"
+                        class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                            <li>
+                                <a href="{{ route('services') }}"
+                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Layanan
+                                    Masyarakat</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('puskesos') }}"
+                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Puskesos</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
                 <x-nav-link href="{{ route('products') }}" :active="request()->routeIs('products') || request()->routeIs('products.*')">Pasar Desa</x-nav-link>
                 <x-nav-link href="{{ route('gallery') }}" :active="request()->routeIs('gallery')">Galeri</x-nav-link>
                 <x-nav-link href="{{ route('articles') }}" :active="request()->routeIs('articles')">Artikel</x-nav-link>
