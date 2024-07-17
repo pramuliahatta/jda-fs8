580d9594b3bf9f5e75a2adefdfd4ff28a51f054a
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-07-17 12:32:06 +0700

    add error message on article category field

--- resources/views/dashboard/articles/create.blade.php
+++ resources/views/dashboard/articles/create.blade.php
@@ -12,30 +12,36 @@
                 @csrf
                 <div class="grid gap-4 mb-4 grid-cols-1">
                     <div>
-                        <x-input-field label="Judul" name="title" id="title" placeholder="Masukkan judul artikel" value="{{ old('title', $article['photo'] ?? '') }}" />
+                        <x-input-field label="Judul" name="title" id="title" placeholder="Masukkan judul artikel"
+                            value="{{ old('title', $article['photo'] ?? '') }}" />
                         <x-error-message field="title" />
                     </div>
 
                     <div>
-                        <x-image-uploader-viewer name="photo" id="photo" imagePath="{{ $photo->image_path ?? '' }}" altText="Foto" />
+                        <x-image-uploader-viewer name="photo" id="photo"
+                            imagePath="{{ $photo->image_path ?? '' }}" altText="Foto" />
                         <x-error-message field="photo" />
                     </div>
 
-                    <label for="category" class="block text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
-                    <select id="category" name="category" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <label for="category"
+                        class="block text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
+                    <select id="category" name="category"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option selected value="" disabled>Pilih kategori</option>
                         <option value="Berita">Berita</option>
                         <option value="Acara">Acara</option>
                     </select>
+                    <x-error-message field="category" />
 
                     <div>
                         @php
-                        // Example values
-                        $article = [
-                        'body' => 'Masukkan konten artikel',
-                        ];
+                            // Example values
+                            $article = [
+                                'body' => 'Masukkan konten artikel',
+                            ];
                         @endphp
-                        <x-textarea-editor id="body" name="body" label="Artikel" value="{{ old('body', $article['body']) }}" />
+                        <x-textarea-editor id="body" name="body" label="Artikel"
+                            value="{{ old('body', $article['body']) }}" />
                         <x-error-message field="body" />
                     </div>
                 </div>
