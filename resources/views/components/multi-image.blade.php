2e1ebbb1074cde08a873eb9f117d8786996ec4c8
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-07-17 12:46:50 +0700

    fix focus ring color on seller products form field

--- resources/views/components/multi-image.blade.php
+++ resources/views/components/multi-image.blade.php
@@ -4,7 +4,7 @@
         {{ $altText }}
     </label>
     <input type="file" name="{{ $name }}[]" id="{{ $id }}" accept="image/*" multiple
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
         onchange="previewImages(event, '{{ $id }}-preview-container')">
     <x-error-message field="photos" />
 
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -44,7 +44,7 @@
                     <label for="category"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
                     <select name="category" id="category"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         <option disabled selected value="">Pilih Kategori</option>
                         <option value="Pakaian">Pakaian</option>
                         <option value="Makanan">Makanan</option>
