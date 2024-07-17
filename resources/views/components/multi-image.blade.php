b701c2936c40283421b68a3fc80455294c140971
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-07-17 12:41:49 +0700

    add error message on product category and image field

--- resources/views/components/multi-image.blade.php
+++ resources/views/components/multi-image.blade.php
@@ -6,6 +6,7 @@
     <input type="file" name="{{ $name }}[]" id="{{ $id }}" accept="image/*" multiple
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
         onchange="previewImages(event, '{{ $id }}-preview-container')">
+    <x-error-message field="photos" />
 
     <div id="{{ $id }}-preview-container" class="mt-4 flex space-x-2">
         @foreach ($imagePaths as $imagePath)
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="title">Pasar Desa</x-slot>
     <section class="bg-white dark:bg-gray-900">
         <div class="py-16 px-16 mx-auto max-w-screen-md sm:py-16 lg:px-16 ">
-            <x-icon-link route="{{'products.index'}}" text="Kembali" />
+            <x-icon-link route="{{ 'products.index' }}" text="Kembali" />
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
                 Tambah Produk
             </h2>
@@ -30,27 +30,33 @@
 
 
                 <div class="w-full mt mb-2">
-                    <x-input-field label="Nama Produk" name="name" id="name" placeholder="Masukkan nama produk" value="{{ old('name', $product->name ?? '') }}" />
+                    <x-input-field label="Nama Produk" name="name" id="name" placeholder="Masukkan nama produk"
+                        value="{{ old('name', $product->name ?? '') }}" />
                     <x-error-message field="name" />
                 </div>
                 <div class="w-full mt-2 mb-2">
 
-                    <x-input-field type="number" label="Harga" name="price" id="price" placeholder="Masukkan harga produk" value="{{ old('price', $photo->price ?? '') }}" />
+                    <x-input-field type="number" label="Harga" name="price" id="price"
+                        placeholder="Masukkan harga produk" value="{{ old('price', $photo->price ?? '') }}" />
                     <x-error-message field="price" />
                 </div>
                 <div>
-                    <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
-                    <select name="category" id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                    <label for="category"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
+                    <select name="category" id="category"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         <option disabled selected value="">Pilih Kategori</option>
                         <option value="Pakaian">Pakaian</option>
                         <option value="Makanan">Makanan</option>
                         <option value="Minuman">Minuman</option>
                         <option value="Lainnya">Lainnya</option>
                     </select>
+                    <x-error-message field="category" />
                 </div>
                 <div class="sm:col-span-2 mt-2 mb-2">
 
-                    <x-textarea-editor id="description" name="description" label="Deskripsi Produk" value="
+                    <x-textarea-editor id="description" name="description" label="Deskripsi Produk"
+                        value="
                             Masukkan deskripsi produk
                             {{-- {{ old('description', $article['body']) }} --}}
                              " />
